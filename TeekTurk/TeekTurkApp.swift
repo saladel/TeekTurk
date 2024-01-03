df01cc01945527a06bc0d26ac59f9838570cbd6d
Adewale Sanusi <adewalesanusi.a@gmail.com>
2024-01-03 17:00:11 +0100

    Initial Commit

---
+++ TeekTurk/TeekTurkApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeekTurkApp.swift
+//  TeekTurk
+//
+//  Created by Adewale Sanusi on 03/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct TeekTurkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
